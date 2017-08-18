3eea00a0900b1867b5724c96b5ba51602832710b
tkkhanye <tkkhanye@gmail.com>
2017-08-18 12:59:28 +0200

    Add files via upload

---
+++ CoreAPI/Startup.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using CoreContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using CoreConcerns.Security;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using System.Security.Principal;
+using CoreAPI.Configs;
+
+namespace CoreAPI
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            // Add framework services.
+            services.AddDbContext<RepoContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddTransient<IPrincipal>(provider => provider.GetService<IHttpContextAccessor>().HttpContext.User);
+
+            //services.AddIdentity<ApplicationUser, IdentityRole>()
+            //    .AddEntityFrameworkStores<ApplicationDbContext>()
+            //    .AddDefaultTokenProviders();
+
+            services.InjectRepositories();
+            services.InjectBusinessLogic();
+            services.InjectCrossCuttingConcerns();
+
+            services.AddCors(options => options.AddPolicy("Cors", builder =>
+            {
+                builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            }));
+
+            services.AddMvc();
+            services.AddAutoMapper();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
+
+            app.UseJwtBearerAuthentication(new JwtBearerOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = SecurityConcern.SigningKey,
+                    ValidateLifetime = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                }
+            });
+
+            app.UseCors("Cors");
+
+            //app.Use(async (context, next) =>
+            //{
+            //    SecurityConcern.InitLoggedInUserId(((Microsoft.AspNetCore.Http.DefaultHttpContext)context)?.User?.Claims?.Where(x => x.Subject is System.Security.Claims.ClaimsIdentity)?.FirstOrDefault()?.Value);
+            //    await next.Invoke();
+            //});
+
+            app.UseMvc();
+        }
+    }
+}
