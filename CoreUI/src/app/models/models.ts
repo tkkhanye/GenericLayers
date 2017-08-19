45c6862ec8f75503df94aae09c1fec66c7f9a2b5
tkkhanye <tkkhanye@gmail.com>
2017-08-19 09:36:40 +0200

    Cleaner UI Models

--- CoreUI/src/app/models/models.ts
+++ CoreUI/src/app/models/models.ts
@@ -17,57 +17,9 @@ export class UserModel{
     establishmentType:number;
 }
 
-export class DealerModel{
-    code: string;
-    name: string;
-    physicalAddress: AddressModel;
-    postalAddress: AddressModel;
-}
-
 export class LookupModel{
     id: number;
     name: string;
     description: string;
     code: string;    
-}
-
-export class VehicleModel{
-    id:string;
-    year:number;
-    registrationNumber:string;
-    vinNumber:string;
-    startMileage:number;
-    customerId:string;
-    vehicleModelId:number;
-    vehicleManufacturerId:number;
-    vehicleMakeId: number;
-    vehicleCategoryId:number;
-    vehicleModel:LookupModel;
-    vehicleMake:LookupModel;
-    vehicleManufacturer:LookupModel;
-    purchasePrice:number;
-}
-
-export class PolicyModel{
-    id:string;
-    startDate:Date;
-    endDate:Date;
-    startMileage:number;
-    endMileage:number;
-    contractDistance:number;
-    productId:string;
-    vehicleId:string;    
-}
-
-export class ProductModel{
-    id:string;
-    name:string;
-    description:string;
-    startDate:Date;
-    endDate:Date;
-    minimumMileage:number;
-    maximumMileage:number;
-    contractDistance:number;
-    contractDuration:number;
-    productType:number;
 }
